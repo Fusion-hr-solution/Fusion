72bfdc34de5909d88bd092629a0674fe68f6851e
Wassim Triki <wsmtriki@gmail.com>
2026-03-12 23:17:58 +0100

    feat(corehr): add initial CoreHR migration

---
+++ Backend/EY.HRPlatform.CoreHR/Infrastructure/Persistence/Migrations/20260312221232_InitialCoreHR.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EY.HRPlatform.CoreHR.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCoreHR : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE SCHEMA IF NOT EXISTS corehr;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
