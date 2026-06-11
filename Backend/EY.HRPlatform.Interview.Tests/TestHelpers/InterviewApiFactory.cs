a8cf3e76cf2d45d5a8e8662765b3c27b5af1545f
Wassim Triki <wsmtriki@gmail.com>
2026-06-11 16:44:10 +0100

    fix(interview): isolate integration test hosts

--- Backend/EY.HRPlatform.Interview.Tests/TestHelpers/InterviewApiFactory.cs
+++ Backend/EY.HRPlatform.Interview.Tests/TestHelpers/InterviewApiFactory.cs
@@ -8,17 +8,13 @@ public class InterviewApiFactory : WebApplicationFactory<Program>
 {
     private readonly string _databaseName = $"InterviewIntegration_{Guid.NewGuid()}";
 
-    public InterviewApiFactory()
-    {
-        Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Testing");
-        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Testing");
-        Environment.SetEnvironmentVariable("ConnectionStrings__InterviewDb", "Server=(localdb)\\mssqllocaldb;Database=InterviewTests;Trusted_Connection=True;");
-        Environment.SetEnvironmentVariable("Database__AutoMigrate", "false");
-        Environment.SetEnvironmentVariable("Database__InMemoryName", _databaseName);
-    }
-
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+        builder.UseSetting("ASPNETCORE_ENVIRONMENT", "Testing");
+        builder.UseSetting("DOTNET_ENVIRONMENT", "Testing");
+        builder.UseSetting("Jwt:Secret", "integration-test-secret-please-change");
+        builder.UseSetting("Database:AutoMigrate", "false");
+        builder.UseSetting("Database:InMemoryName", _databaseName);
     }
 }
